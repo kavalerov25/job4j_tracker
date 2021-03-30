2dce38600ddcddf17ff6fceac93137d9831018f9
kavalerov25 <paradise_777@mail.ru>
2021-03-30 10:56:10 +0300

    7. Date. Отображение даты. [#454404] исправил

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,6 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(timeFormatter.format(item.getCreated()));
+        System.out.println(item.getCreated().format(timeFormatter));
     }
 }
