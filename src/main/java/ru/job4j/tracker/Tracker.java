420b4cf10b9b52de277124711809e92adfe9f671
kavalerov25 <paradise_777@mail.ru>
2021-04-02 22:09:24 +0300

    6. Метод замены заявки. Tracker.replace [#454412]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -22,23 +22,40 @@ public class Tracker {
         int count = 0;
         for (int i = 0; i < size; i++) {
             if (this.items[i].getName().equals(key)) {
-                        itemsNames[count] = this.items[i];
-                        count++;
-                    }
+                itemsNames[count] = this.items[i];
+                count++;
             }
+        }
         itemsNames = Arrays.copyOf(itemsNames, count);
         return itemsNames;
-        }
+    }
 
-        public Item findById(int id) {
-            Item rsl = null;
-            for (int index = 0; index < size; index++) {
-                Item item = items[index];
-                if (item.getId() == id) {
-                    rsl = item;
-                    break;
-                }
+    public Item findById(int id) {
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
             }
-            return rsl;
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        if (indexOf(id) >= 0) {
+            int index = indexOf(id);
+            item.setId(id);
+            items[index] = item;
+            return true;
+        } else {
+            return false;
         }
     }
+}
