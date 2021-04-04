bc9661ac81c5c8d9bf71ed2380e309edf82d6384
kavalerov25 <paradise_777@mail.ru>
2021-04-04 22:12:24 +0300

    Revert "7. Метод удаления заявки Tracker.delete [#454413]"
    This reverts commit a740b63c

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,15 +57,4 @@ public class Tracker {
         }
         return false;
     }
-
-    public boolean delete(int id) {
-        int index = indexOf(id);
-        if (index >= 0) {
-            System.arraycopy(items, index + 1, items, index, size - index);
-            items[size - 1] = null;
-            size--;
-            return true;
-        }
-        return false;
-    }
 }
