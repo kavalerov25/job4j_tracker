37b4c8fb61fd0751beb18f4739cd2542e52619b3
kavalerov25 <paradise_777@mail.ru>
2021-04-05 11:20:31 +0300

    Исправил метод Delete

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,13 +60,13 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean result = index != -1;
-        if (result) {
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
-        return result;
+        return rsl;
     }
 }
 
