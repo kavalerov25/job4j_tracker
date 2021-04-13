793b884705fe9ae957a5f01d32a732e955c4dace
kavalerov25 <paradise_777@mail.ru>
2021-04-13 12:02:17 +0300

    подкорректировал

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -4,12 +4,8 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return    "|-----|  " + ln
+        return "|-----|  " + ln
                 + "|     | " + ln
                 + "|_____|" + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
     }
 }
