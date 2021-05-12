f4735b054d65724eb680b0901d1ef93067a558a7
kavalerov25 <paradise_777@mail.ru>
2021-05-12 21:51:01 +0300

    2. Идентичные тексты [#212655 #20797]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        /* for-each origin -> new HashSet. */
+        for (String word : origin) {
+            check.add(word);
+        }
+        /* for-each text -> hashSet.contains */
+        for (String duplicate :text) {
+            if (!check.contains(duplicate)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
