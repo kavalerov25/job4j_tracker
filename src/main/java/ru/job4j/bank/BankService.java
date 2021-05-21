fe476ded281088bdfd073c743cdf5209042a05b3
kavalerov25 <paradise_777@mail.ru>
2021-05-22 00:00:05 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,60 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<>());
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> list = users.get(user);
+            if (!list.contains(account)) {
+                list.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        List<Account> listAccounts = users.get(this.findByPassport(passport));
+        if (listAccounts != null) {
+            for (Account account : listAccounts) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account firstAccount = findByRequisite(srcPassport, srcRequisite);
+        Account secondAccount = findByRequisite(destPassport, destRequisite);
+        if (firstAccount != null && secondAccount != null && firstAccount.getBalance() >= amount) {
+        firstAccount.setBalance(firstAccount.getBalance() - amount);
+        secondAccount.setBalance(secondAccount.getBalance() + amount);
+        return true;
+    }
+       return false;
+}
+}
+
