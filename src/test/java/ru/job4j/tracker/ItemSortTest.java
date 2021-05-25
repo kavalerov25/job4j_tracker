28cf10656af0e0908a04456b65a152a43a34d52a
kavalerov25 <paradise_777@mail.ru>
2021-05-25 23:23:53 +0300

    fix

--- src/test/java/ru/job4j/tracker/ItemSortTest.java
+++ src/test/java/ru/job4j/tracker/ItemSortTest.java
@@ -12,32 +12,39 @@ public class ItemSortTest {
 
     @Test
     public void whenSortByName() {
-        Item item1 = new Item("D");
-        Item item2 = new Item("C");
-        Item item3 = new Item("B");
-        Item item4 = new Item("A");
-        List<Item> input = Arrays.asList(item1, item2, item3, item4);
+        List<Item> input = Arrays.asList(
+        new Item("D"),
+        new Item("C"),
+        new Item("B"),
+        new Item("A")
+        );
+        List<Item> expect = Arrays.asList(
+                new Item("A"),
+                new Item("B"),
+                new Item("C"),
+                new Item("D")
+        );
         input.sort(new ItemSortByName());
-        assertEquals(input.get(0).getName(), "A");
-        assertEquals(input.get(1).getName(), "B");
-        assertEquals(input.get(2).getName(), "C");
-        assertEquals(input.get(3).getName(), "D");
-
+        assertThat(input, is(expect));
     }
 
     @Test
     public void whenSortByNameRes() {
-        Item item1 = new Item("D");
-        Item item2 = new Item("C");
-        Item item3 = new Item("B");
-        Item item4 = new Item("A");
-        Item item5 = new Item("E");
-        List<Item> input = Arrays.asList(item1, item2, item3, item4, item5);
+        List<Item> input = Arrays.asList(
+        new Item("D"),
+        new Item("C"),
+        new Item("B"),
+        new Item("A"),
+        new Item("E")
+        );
+        List<Item> expect = Arrays.asList(
+                new Item("E"),
+                new Item("D"),
+                new Item("C"),
+                new Item("B"),
+                new Item("A")
+        );
         input.sort(new ItemSortByNameReservsOrder());
-        assertEquals(input.get(0).getName(), "E");
-        assertEquals(input.get(1).getName(), "D");
-        assertEquals(input.get(2).getName(), "C");
-        assertEquals(input.get(3).getName(), "B");
-        assertEquals(input.get(4).getName(), "A");
+        assertThat(input, is(expect));
     }
 }
