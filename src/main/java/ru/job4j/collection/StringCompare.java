b6d8da0d4663b22643b7dccaa143f2b1b086e7ff
kavalerov25 <paradise_777@mail.ru>
2021-05-28 19:37:37 +0300

    Fix

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -11,7 +11,7 @@ public class StringCompare implements Comparator<String> {
                     return Integer.compare(left.charAt(i), right.charAt(i));
                 }
             }
-            return left.length() - right.length();
+            return Integer.compare(left.length(), right.length());
         }
     }
 
