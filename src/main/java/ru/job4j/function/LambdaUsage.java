124f5f9e502bce2bfff90a989afedcd70fd40897
kavalerov25 <paradise_777@mail.ru>
2021-06-13 20:42:16 +0300

    fix

--- src/main/java/ru/job4j/function/LambdaUsage.java
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -1,12 +1,16 @@
 package ru.job4j.function;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
     public static void main(String[] args) {
+        String[] strings = {"один", "два", "тройка", "четыре", "пять" };
         Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + right.length() + " : " + left.length());
-            return right.length() - left.length();
+            return Integer.compare(right.length(), left.length());
         };
+        Arrays.sort(strings, cmpDescSize);
+        System.out.println(Arrays.asList(strings));
     }
 }
