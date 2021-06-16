25422c4a3064613eab2163174c078954590b3417
kavalerov25 <paradise_777@mail.ru>
2021-06-16 19:36:17 +0300

    fix

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -12,9 +12,11 @@ public class FI {
         };
 
         Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
-        Comparator<String> cmpSize = (left, right) -> left.length() - right.length();
+        Comparator<String> cmpSize = (left, right) ->
+                Integer.compare(left.length(), right.length());
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+        Comparator<String> cmpDescSize = (left, right) ->
+                Integer.compare(right.length(), left.length());
         Arrays.sort(atts, comparator);
         for (Attachment a : atts) {
             System.out.println(a);
