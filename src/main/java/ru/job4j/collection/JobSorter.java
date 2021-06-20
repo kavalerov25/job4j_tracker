5309995404d9380d740078a56bc5f614a1c5fdc2
kavalerov25 <paradise_777@mail.ru>
2021-06-20 23:01:50 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -28,8 +28,15 @@ public class JobSorter {
         Collections.sort(jobs, new SortByNameJob());
         System.out.println(jobs);
         */
-        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
+      //  Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
+      // System.out.println(jobs);
+        jobs.sort(new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
 
+        Comparator<Job> compareName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparePriority = Comparator.comparingInt(Job::getPriority);
+        Comparator<Job> combine = compareName.thenComparing(comparePriority);
+
+        jobs.sort(combine);
     }
 }
