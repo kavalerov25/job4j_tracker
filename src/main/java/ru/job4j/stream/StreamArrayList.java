f1fb64772274c6830b3228f26341e1cc203b59d3
kavalerov25 <paradise_777@mail.ru>
2021-06-21 21:24:43 +0300

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/StreamArrayList.java
+++ src/main/java/ru/job4j/stream/StreamArrayList.java
@@ -8,7 +8,9 @@ import java.util.stream.Collectors;
 public class StreamArrayList {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(-5, -3, -1, 0, 1, 3, 5));
-        List<Integer> list = arrayList.stream().filter(x -> x >= 0).collect(Collectors.toList());
+        List<Integer> list = arrayList.stream()
+                .filter(x -> x >= 0)
+                .collect(Collectors.toList());
         System.out.println(list);
     }
 }
