985e9fd33d67e880ced6e1dc5715fb9e9fb43316
kavalerov25 <paradise_777@mail.ru>
2021-07-04 14:02:39 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -14,7 +14,7 @@ import java.util.Map;
  * @version 1.0
  */
 public class BankService {
-    private Map<User, List<Account>> users = new HashMap<>();
+    private final Map<User, List<Account>> users = new HashMap<>();
 
     /**
      * Метод позволяет добавить объект класса User в поле users,
@@ -45,32 +45,31 @@ public class BankService {
 
     /**
      * Метод проверяет наличие пользователя в системе по его паспорту
+     *
      * @param passport
      * @return возвращает пользователя или null если такого пользователя нет
      */
     public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet().stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Метод ищет счет пользователя по реквизитам счета и паспорту пользователя
+     *
      * @param passport
      * @param requisite
      * @return возвращает счет пользователя или null если такого счета нет
      */
     public Account findByRequisite(String passport, String requisite) {
-        List<Account> listAccounts = users.get(this.findByPassport(passport));
-        if (listAccounts != null) {
-            for (Account account : listAccounts) {
-                if (account.getRequisite().equals(requisite)) {
-                    return account;
-                }
-            }
+        User user = findByPassport(passport);
+        if (user != null) {
+            return users.get(user).stream()
+                    .filter(account -> account.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
@@ -78,13 +77,14 @@ public class BankService {
     /**
      * Метод позволяет осуществить перевод денежных средств
      * с одного объекта класса Account к другому.
-     * @param srcPassport - паспорт User от которого будет осуществляться перевод
-     * @param srcRequisite - реквизиты конкретного объекта класса Account.
-     * Счет откуда будут списываться деньги
-     * @param destPassport - паспорт User которому будет осуществляться перевод
+     *
+     * @param srcPassport   - паспорт User от которого будет осуществляться перевод
+     * @param srcRequisite  - реквизиты конкретного объекта класса Account.
+     *                      Счет откуда будут списываться деньги
+     * @param destPassport  - паспорт User которому будет осуществляться перевод
      * @param destRequisite - реквизиты конкретного объекта класса Account.
-     * Счет на который будут зачисляться деньги
-     * @param amount - сумма которую необходимо перевести.
+     *                      Счет на который будут зачисляться деньги
+     * @param amount        - сумма которую необходимо перевести.
      * @return boolean - в случае успешного перевода,
      * метод возвращает true. В случае неудачного перевода
      * метод вернет false.
