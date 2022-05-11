87b001514b46d83aad732b6663f64e8fe402b116
kavalerov25 <paradise_777@mail.ru>
2022-05-11 11:50:56 +0300

    1. Реализация кеша на SoftReference [#1592] в get используйте уже написанный метод put

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -9,15 +9,14 @@ public abstract class AbstractCache<K, V> {
     private final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-        SoftReference<V> ref = new SoftReference<>(value);
-        cache.put(key, ref);
+        cache.put(key, new SoftReference<>(value));
     }
 
     public V get(K key) {
         V value = cache.getOrDefault(key, new SoftReference<>(null)).get();
         if (value == null) {
             value = load(key);
-            cache.put(key, new SoftReference<>(value));
+            put(key,value);
         }
         return value;
     }
