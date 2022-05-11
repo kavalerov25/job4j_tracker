306771df9d26b3113cd1b1054e933287389f392a
kavalerov25 <paradise_777@mail.ru>
2022-05-11 20:56:35 +0300

    1. Реализация кеша на SoftReference [#1592] исправления

--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -3,34 +3,33 @@ package ru.job4j.cache;
 import java.util.Scanner;
 
 public class Emulator {
-    private final DirFileCache dirFileCache;
 
-    public Emulator(String directoryCache) {
-        dirFileCache = new DirFileCache(directoryCache);
-    }
+    private final static Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Укажите директорию с файлами");
-        String directoryName = scanner.next();
-        DirFileCache cache = new DirFileCache(directoryName);
-        boolean run = true;
-        while (run) {
-            System.out.println("Укажите имя файла");
-            String fileName = scanner.next();
-            System.out.println("Нажмите 1 для загрузки содержимого в кэш" + System.lineSeparator()
-                               + "Нажмите 2 для получения содержимого из кэша");
-            int task = scanner.nextInt();
-            if (task == 1) {
-                cache.put(fileName, cache.load(fileName));
-            } else if (task == 2) {
-                System.out.println(cache.get(fileName));
+        String directoryName = SCANNER.nextLine();
+        DirFileCache dirFileCache = new DirFileCache(directoryName);
+        System.out.println("Введите название файла ");
+        String fileName = SCANNER.nextLine();
+        System.out.println("Введите что вы хотите: ");
+        System.out.println("Нажмите 1 для загрузки содержимого в кэш" + System.lineSeparator()
+                           + "Нажмите 2 для получения содержимого из кэша" + System.lineSeparator()
+                           + "Нажмите 3 для выхода");
+        int task = SCANNER.nextInt();
+        switch (task) {
+            case 1 -> {
+                dirFileCache.put(fileName, dirFileCache.load(fileName));
+                break;
             }
-            System.out.println("Нажмите 3 для выхода");
-            int escape = scanner.nextInt();
-            if (escape == 3) {
-                run = false;
+            case 2 -> {
+                System.out.println(dirFileCache.get(fileName));
+                break;
             }
+            case 3 -> {
+                break;
+            }
+            default -> System.out.println("Введите заново");
         }
     }
 }
