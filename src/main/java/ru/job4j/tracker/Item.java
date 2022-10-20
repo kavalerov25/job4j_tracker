72d17602ba39b9da39fc4831c22680f8fb20e889
kavalerov25 <paradise_777@mail.ru>
2022-10-20 19:44:14 +0300

    1. Конфигурация Hibernate [#6872] fix 3

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -62,12 +62,11 @@ public class Item {
         }
         Item item = (Item) o;
         return id == item.id
-               && Objects.equals(name, item.name)
-               && Objects.equals(created, item.created);
+               && Objects.equals(name, item.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, created);
+        return Objects.hash(id, name);
     }
 }
