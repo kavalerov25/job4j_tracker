f4929f9d8575e0bddb28be346bae1e5fbec669ed
kavalerov25 <paradise_777@mail.ru>
2022-11-30 23:07:27 +0300

    5. Date [#331992]

---
+++ src/main/java/ru/job4j/tracker/DateRun.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+public class DateRun {
+    public static void main(String[] args) {
+        var registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (var sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            var session = sf
+                    .withOptions()
+                    .jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+                    .openSession();
+            session.beginTransaction();
+            var item = new Item();
+            item.setName("check timezone");
+            item.setCreated(LocalDateTime.now());
+            session.persist(item);
+            var stored = session.createQuery(
+                    "from Item", Item.class
+            ).list();
+            for (Item it : stored) {
+                var time = it.getCreated().atZone(
+                        ZoneId.of("UTC+3")
+                ).format(DateTimeFormatter.ofPattern("HH:mm yyyy-MM-dd"));
+                System.out.println(time);
+            }
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
