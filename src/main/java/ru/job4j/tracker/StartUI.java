0cc23425be60d697bf258b7228fe35186b449d17
kavalerov25 <paradise_777@mail.ru>
2023-06-21 23:06:07 +0300

    4. Docker сервисы [#504781]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,13 @@
 package ru.job4j.tracker;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class StartUI {
         private  final Output out;
@@ -93,10 +99,38 @@ public class StartUI {
             }
         }
 
+    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
+        String value = System.getenv(sysEnv);
+        if (value == null) {
+            value = config.getProperty(key);
+        }
+        return value;
+    }
+
+    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
+        var config = new Properties();
+        try (InputStream in = StartUI.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
+        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
+        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
+        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
+        System.out.println("url=" + url);
+        Class.forName(driver);
+        return DriverManager.getConnection(
+                url, username, password
+        );
+    }
+
         public static void main(String[] args) {
+
             Output output = new ConsoleOutput();
             Input input = new ValidateInput(output, new ConsoleInput());
-            try (Store tracker = new SqlTracker()) {
+            try (Connection connection = loadConnection()) {
                 ArrayList<UserAction> actions = new ArrayList<>();
             actions.add(new CreateAction(output));
             actions.add(new DeleteAction(output));
@@ -105,6 +139,7 @@ public class StartUI {
             actions.add(new FindByIdAction(output));
             actions.add(new FindByNameAction(output));
             actions.add(new ExitAction(output));
+                Store tracker = new MemTracker();
                 new StartUI(output).init(input, tracker, actions);
             } catch (Exception e) {
                 e.printStackTrace();
